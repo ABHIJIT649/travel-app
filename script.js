562afaa838926ecdd2b41038e3ac8e6bb0bbab49
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-07-31 13:28:45 +0530

    add images file in project

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const header = document.getElementById("hero");
 
 const bgImages = [
-  "htr1.jpg",
-  "htr2.jpg",
-  "htr3.jpg",
-  "htr4.jpg",
-  "htr5.jpg"
+  "images/htr1.jpg",
+  "images/htr2.jpg",
+  "images/htr3.jpg",
+  "images/htr4.jpg",
+  "images/htr5.jpg"
 ];
 
 let currentIndex = 0;
@@ -23,52 +23,52 @@ setInterval(changeBackground, 3000);
 const destinations = [
     {
       name: "Paris, France",
-      image: "paris.jpg",
+      image: "images/paris.jpg",
       description: " is the French word for the number twenty. Vingt is also used in phrases like vingt ans (twenty years) and vingt-et-un(twenty-one) according to Vidalingua and Preply. "
     },
     {
       name: "Bali, Indonesia",
-      image: "indonesia.jpg",
+      image: "images/indonesia.jpg",
       description: "Bali, often called the Island of Gods,is a province of Indonesia known for its volcanic scenery, elaborate dances, and vibrant culture. It's a popular tourist."
     },
     {
       name: "New York, USA",
-      image: "newyork.jpg",
+      image: "images/newyork.jpg",
       description: "New York City: vibrant, bustling, diverse, exciting, expensive, crowded, energetic, ambitious, gritty, captivating, cultural, innovative, global   . "
     },
     {
       name: "Florida, USA",
-      image: "disneyland.jpg",
+      image: "images/disneyland.jpg",
       description: "Florida, also known as the Sunshine State, is famous for its beautiful beaches, vibrant nightlife, and unique cultural experiences. It's the southeasternmost state in the US, "
     },
     {
       name: " Ladakh",
-      image: "ladak.jpg",
+      image: "images/ladak.jpg",
       description: "Ladakh, the Land of High Passes, is a breathtaking region in the Himalayas known for its rugged beauty, ancient monasteries, and vibrant Buddhist culture. "
     },
     {
       name: "Goa",
-      image: "goa.avif",
+      image: "images/goa.avif",
       description: "Popularly known as India’s party capital, Goa seduces travellers from all around the globe with its boho beaches, ancient churches, majestic forts and unbeatable nightlife!"
     },
     {
       name: "Manali",
-      image: "manali.avif",
+      image: "images/manali.avif",
       description: "Nestled on the banks of River Beas, Manali will take your breath away with its snow-kissed peaks, lush valleys and heart-pumping adventure experiences in the heart of Himalayas."
     },
     {
       name: "Krabi",
-      image: "krabi.avif",
+      image: "images/krabi.avif",
       description: "Krabi is abundant in scenic beauty with lush tropical greenery, rugged limestone cliffs, meandering caves and a stunning coastline with some of the best beaches in the world.."
     },
     {
       name: "Bali",
-      image: "bali.webp",
+      image: "images/bali.webp",
       description: "An exotic tropical destination that flaunts scenic beaches and mountains, Bali is deep-rooted in culture and tradition, which adds to the charm of this scenic isla"
     },
     {
       name: "laksadweep",
-      image: "laksadweep.webp",
+      image: "images/laksadweep.webp",
       description: "Lakshadweep, India's smallest Union Territory, is an archipelago of 36 islands known for its stunning beaches, vibrant coral reefs, and rich marine life."
     },
   ];
